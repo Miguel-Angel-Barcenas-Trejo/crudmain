97d8abe4c37220bd3fa3b09c819fc68300e4cbd1
Miguel Angel Barcenas Trejo <142522658+Miguel-Angel-Barcenas-Trejo@users.noreply.github.com>
2024-10-21 23:42:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Configuración de la base de datos
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://prueba4_jb9x_user:KGFHiYDG5hRPERRpfZHzwG9JVmh9GIjj@dpg-csbikajtq21c73a142b0-a/prueba4_jb9x'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
